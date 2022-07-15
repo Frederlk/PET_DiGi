a5ccb39ee29569e02563f17e37aa75821d2aa32f
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-15 17:24:17 +0300

    installed packages \ started updating to v3 of template \ cleared scss files

--- #src/js/files/script.js
+++
@@ -1,5 +0,0 @@
-var grid = document.querySelector('.news__masonry');
-var colc = new Colcade(grid, {
-   columns: '.news__grid-column',
-   items: '.news__grid-item'
-});
---
+++ src/js/files/script.js
@@ -0,0 +1,10 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+var grid = document.querySelector(".news__masonry");
+var colc = new Colcade(grid, {
+    columns: ".news__grid-column",
+    items: ".news__grid-item",
+});
