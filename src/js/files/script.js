263245c88110da71a1b64d704199c3196ab16671
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-15 19:24:26 +0300

    scss - updated constants \ footer v2 \ header v2

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,8 +3,8 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-var grid = document.querySelector(".news__masonry");
-var colc = new Colcade(grid, {
-    columns: ".news__grid-column",
-    items: ".news__grid-item",
-});
+// var grid = document.querySelector(".news__masonry");
+// var colc = new Colcade(grid, {
+//     columns: ".news__grid-column",
+//     items: ".news__grid-item",
+// });
