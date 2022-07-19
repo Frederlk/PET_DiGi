c8b62e193123f6732694e1efdabf5a7daa4a8a45
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-19 16:22:57 +0300

    news v2

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,10 +1,11 @@
 // Подключение функционала "Чертогов Фрилансера"
+import Masonry from "masonry-layout";
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-// var grid = document.querySelector(".news__masonry");
-// var colc = new Colcade(grid, {
-//     columns: ".news__grid-column",
-//     items: ".news__grid-item",
-// });
+let msnry = new Masonry(".news__masonry", {
+    itemSelector: ".news__grid-item",
+    gutter: 33,
+    fitWidth: true,
+});
